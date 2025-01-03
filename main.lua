8200015e3b0aeb7e9a6ccef27369e3777606b601
AFanOfHololive <ios2004@icloud.com>
2025-01-03 16:41:22 +0700

    refactor: refactored the playre code and print stats

--- main.lua
+++ main.lua
@@ -4,16 +4,37 @@ function love.load()
 	Window_width = 500
 	Window_height = 800
 	Player = NewPlayer()
+
+	love.window.setTitle("yayya")
 	love.window.setMode(Window_width, Window_height)
 	love.keyboard.setKeyRepeat(true)
 end
 
 function love.update(dt)
 	Player:update(dt)
+	for i, bullet in ipairs(Player.bullets) do
+		bullet:update(dt)
+		if bullet.y < 0 then
+			table.remove(Player.bullets, i)
+		end
+	end
 end
 
 function love.draw()
+	Draw_stats()
+	for _, bullet in pairs(Player.bullets) do
+		love.graphics.draw(bullet.sprite, bullet.x,bullet.y,0,1,1,0,32)
+	end
 	if Player ~= nil then
 		love.graphics.draw(Player.sprite,Player.x,Player.y,0,1,1,0,32)
+	end
+end
+
+function Draw_stats()
+	local stats = love.graphics.getStats()
+	local stat_location = 0
+	for k,v in pairs(stats) do
+		love.graphics.print(k .. ":" .. v .. "\n", 10, stat_location)
+		stat_location = stat_location + 20
 	end
 end
--- player.lua
+++ player.lua
@@ -1,48 +1,56 @@
+require("bullet")
 function NewPlayer()
 	local player = {
 		sprite = love.graphics.newImage("assets/PipHeart.png"),
+		bullets = {},
 		x = 10,
 		y = 10,
 		speed = 350,
 		focus_speed = 150,
 		width = 50,
 		height = 50,
-
-		update = function(self,dt)
-			self:move(dt)
-		end,
-
-		move = function(self, dt)
-			local move_speed = self.speed
-			if love.keyboard.isDown(Keymaps.slow_mode) then move_speed = self.slow_speed end
-
-			local dir = {x=0,y=0}
-			dir = self:get_move_direction()
-			self.x = self.x + dir.x * dt * move_speed
-			self.y = self.y + dir.y * dt * move_speed
-
-			--! set walls
-			if self.x < 0 - self.width/4 then self.x = 0 - self.width/4 end
-			if self.x > Window_width - self.width then self.x = Window_width - self.width end
-
-			if self.y < 0 + self.height/2 then self.y = 0 + self.height/2 end
-			if self.y > Window_height - self.height/2 then self.y = Window_height - self.height/2 end
-		end,
-
-		get_move_direction = function()
-			local dir = {x=0,y=0}
-			if love.keyboard.isDown(Keymaps.right) then dir.x = dir.x + 1 end
-			if love.keyboard.isDown(Keymaps.left) then dir.x = dir.x - 1 end
-			if love.keyboard.isDown(Keymaps.up) then dir.y = dir.y - 1 end
-			if love.keyboard.isDown(Keymaps.down) then dir.y = dir.y + 1 end
-
-			local len = math.sqrt(dir.x^2 + dir.y^2)
-			if len > 0 then
-				dir.x = dir.x/len
-				dir.y = dir.y/len
-			end
-			return dir
-		end,
 	}
+	function player:update(dt)
+		if love.keyboard.isDown(Keymaps.attack) then
+			self:shoot()
+		end
+		self:move(dt)
+	end
+
+	function player:shoot()
+		table.insert(self.bullets, NewBullet("/assets/test-bullet.png", self.x + self.width/2, self.y,200,{x=0,y=-1}))
+	end
+
+	function player:move(dt)
+		local move_speed = self.speed
+		if love.keyboard.isDown(Keymaps.slow_mode) then move_speed = self.focus_speed end
+
+		local dir = {x=0,y=0}
+		dir = self:get_move_direction()
+		self.x = self.x + dir.x * dt * move_speed
+		self.y = self.y + dir.y * dt * move_speed
+
+		--! set walls
+		if self.x < 0 - self.width/4 then self.x = 0 - self.width/4 end
+		if self.x > Window_width - self.width then self.x = Window_width - self.width end
+
+		if self.y < 0 + self.height/2 then self.y = 0 + self.height/2 end
+		if self.y > Window_height - self.height/2 then self.y = Window_height - self.height/2 end
+	end
+
+	function player:get_move_direction()
+		local dir = {x=0,y=0}
+		if love.keyboard.isDown(Keymaps.right) then dir.x = dir.x + 1 end
+		if love.keyboard.isDown(Keymaps.left) then dir.x = dir.x - 1 end
+		if love.keyboard.isDown(Keymaps.up) then dir.y = dir.y - 1 end
+		if love.keyboard.isDown(Keymaps.down) then dir.y = dir.y + 1 end
+
+		local len = math.sqrt(dir.x^2 + dir.y^2)
+		if len > 0 then
+			dir.x = dir.x/len
+			dir.y = dir.y/len
+		end
+		return dir
+	end
 	return player
 end
